67bd0636243009754e61f8e0076df73e2513a04d
pavanmadhur <pavanmadhurjune30@gmail.com>
2024-01-10 17:28:13 +0530

    sd

--- app.py
+++ app.py
@@ -35,7 +35,7 @@ def prediction():
 def showdata():
     conn = sqlite3.connect('cropdata.db')
     cur = conn.cursor()
-    cur.execute("SELECT * FROM crop")
+    cur.execute("SELECT * FROM Crop prediction")
     x = cur.fetchall()
     li  = []
     for i in x:
